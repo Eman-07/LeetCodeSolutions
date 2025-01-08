e29fa3452eca6ef28f7917a8853bcb5e645a6aa6
Muhammad Eman <memanmaan44@gmail.com>
2025-01-09 00:05:04 +0500

    updated

---
+++ Java/_3LongestSubstring.java
@@ -0,0 +1,18 @@
+public class _3LongestSubstring {
+
+    public static void main(String[] args) {
+
+        String s = "abcabcd";
+        String subString;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            for (int j = i + 1; j < s.length(); j++) {
+                
+            }
+
+        }
+
+
+    }
+}
