d0917c949b014149f3697b202e9d2bbd2da1919e
Muhammad Eman <memanmaan44@gmail.com>
2025-01-18 00:00:01 +0500

    solved leetcode problem 9

---
+++ Java/out/production/LeetCodeSolutions/_9isPalindrome.java
@@ -0,0 +1,29 @@
+package out.production.LeetCodeSolutions;
+
+public class _9isPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1000021));
+        }
+
+    public  static boolean isPalindrome(int x) {
+
+        Integer i = x;
+        String s =i.toString();
+        char c[] = s.toCharArray();
+        boolean status= false;
+
+        for (int j = 0; j <c.length; j++){
+            if (c[j] == c[c.length-j-1]){
+                status = true;
+            }else{
+                status = false;
+                break;
+            }
+        }
+        return status;
+    }
+
+
+}
+
